614ff20e0063c61a1690090c26d750ee75a05cd2
lmk-kevin <kevin@lmksystem.fr>
2023-12-14 16:48:21 +0100

    Clean code avant soutenance

--- src/api/getter.js
+++ src/api/getter.js
@@ -43,13 +43,6 @@ export const getAverageByUserId = async (userId) => {
 export const getPerfByUserId = async (userId) => {
   try {
     const response = await axios.get(`/user/${userId}/performance`);
-    let allKindTitleEng = response.data.data.kind;
-    // 1: 'cardio',
-    // 2: 'energy',
-    // 3: 'endurance',
-    // 4: 'strength',
-    // 5: 'speed',
-    // 6: 'intensity'
     let allKindTitleFr = {
       1:"Cardio",
       2:"Energie",
